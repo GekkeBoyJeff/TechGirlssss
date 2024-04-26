a2485d27c71f62b592c611fbeed7ee8e429a2f2b
Jeffrey Ullers <jeffrey.ullers@hva.nl>
2024-04-26 13:06:56 +0200

    Added dotenv, ejs and nodemon, furthermore I added a base folder structure for pages & partials

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
-import express from 'express';
-import dotenv from 'dotenv';
-dotenv.config();
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
 
-const app = express();
-const port = process.env.port;
+const app = express()
+const port = process.env.port
 
 app.set('view engine', 'ejs')
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 app.get('/', async (req, res) => {
-    res.send("hello");
+    res.render('pages/index');
+});
+
+app.get('/about', async (req, res) => {
+    res.render('pages/about');
 });
 
 app.listen(port, function () {
-    console.log(`Example app listening on port ${port}!`);
+    console.log(`Example app listening on port http://localhost:${port}`)
 });
