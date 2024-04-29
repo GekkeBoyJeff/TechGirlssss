fd4c6a8de7b1cac45c98d9b33ff03bb61f973f0f
GekkeBoyJeff <j.ullers@live.nl>
2024-04-29 04:08:44 +0200

    Made the router shorter and fixed a minor mistake in the loading of scripts

--- routes/routes.js
+++ routes/routes.js
@@ -1,23 +1,15 @@
+const routes = [
+    { path: '/', view: 'pages/index', title: 'Home of techGirlsss', layouts: 'default.ejs', scripts: ['script', 'test'] },
+    { path: '/login', view: 'pages/login', title: 'Login', scripts: null },
+    { path: '/register', view: 'pages/register', title: 'Register', scripts: null },
+    { path: '/about', view: 'pages/about', title: 'About', scripts: null },
+    { path: '/contact', view: 'pages/contact', title: 'Contact', scripts: null }
+];
+
 export default function (app) {
-    app.get('/', async (req, res) => {
-        res.render('pages/index', { title: 'Home of techGirlsss', layouts: 'default.ejs', script: ['script', 'test'] },)
-    })
-
-    app.get('/login', async (req, res) => {
-        res.render('pages/login', { title: 'Login', script: null })
-    })
-
-    app.get('/register', async (req, res) => {
-        res.render('pages/register', { title: 'Register', script: null })
-    })
-
-    app.get('/about', async (req, res) => {
-        res.render('pages/about', { title: 'About', script: null })
-    }
-    )
-
-    app.get('/contact', async (req, res) => {
-        res.render('pages/contact', { title: 'Contact', script: null })
-    }
-    )
+    routes.forEach(route => {
+        app.get(route.path, (req, res) => {
+            res.render(route.view, { title: route.title, layouts: route.layouts, scripts: route.scripts });
+        });
+    });
 }
