84a5a603e0aa4c1b9b89674f6dbdf80c38f0e554
GekkeBoyJeff <j.ullers@live.nl>
2024-05-02 00:00:42 +0200

    Code nog ingewikkelder gemaakt :D

--- routes/routes.js
+++ routes/routes.js
@@ -1,15 +1,15 @@
-const routes = [
-    { path: '/', view: 'pages/index', title: 'Home of techGirlsss', layouts: 'default.ejs', scripts: ['script', 'test'] },
-    { path: '/login', view: 'pages/login', title: 'Login', scripts: null },
-    { path: '/register', view: 'pages/register', title: 'Register', scripts: null },
-    { path: '/about', view: 'pages/about', title: 'About', scripts: null },
-    { path: '/contact', view: 'pages/contact', title: 'Contact', scripts: null }
-];
+const routes = {
+    '/' : { view: 'pages/index', title: 'Home of techGirlsss', layouts: 'default.ejs', scripts: ['script', 'test'] },
+    '/login' : { view: 'pages/login', title: 'Login', scripts: null },
+    '/register' : { view: 'pages/register', title: 'Register', scripts: null },
+    '/about' : { view: 'pages/about', title: 'About', scripts: null },
+    '/contact' : { view: 'pages/contact', title: 'Contact', scripts: null }
+}
 
 export default function (app) {
-    routes.forEach(route => {
-        app.get(route.path, (req, res) => {
-            res.render(route.view, { title: route.title, layouts: route.layouts, scripts: route.scripts });
+    for (const [path, route] of Object.entries(routes)) {  // Object.entries() returns an array of a given object's own enumerable string-keyed property [key, value] pairs
+        app.get(path, (req, res) => { 
+            res.render(route.view, route); // route is an object with view, title, layouts, and scripts properties
         });
-    });
+    }
 }
