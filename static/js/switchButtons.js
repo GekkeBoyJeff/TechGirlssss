0b600ace01db6007bd144b652739bf4e68f4af48
Abby de Best <121155006+abbydebest@users.noreply.github.com>
2024-06-07 00:15:07 +0200

    Trying with JS to create a custom 'active' state on switch buttons

--- static/js/switchButtons.js
+++ static/js/switchButtons.js
@@ -6,8 +6,30 @@ let upcomingEventsButton = document.querySelector(".upcoming-events-button");
 let savedEvents = document.querySelector(".saved-events");
 let upcomingEvents = document.querySelector(".upcoming-events");
 
+let wishlistText = document.querySelector("#wishlistText");
+let eventsText = document.querySelector("#eventsText");
+
+let wishlistSvg = document.querySelector("#wishlistSvg");
+let eventsSvg = document.querySelector("#eventsSvg");
+
 upcomingEventsButton.addEventListener("click", function() {
-    console.log("Switch werkt");
-    upcomingEvents.classList.remove(".hidden");
-    savedEvents.classList.add(".hidden");
+    console.log("Upcoming events werkt");
+    upcomingEvents.classList.remove("hidden");
+    savedEvents.classList.add("hidden");
+    
+    eventsText.classList.add("clicked");
+    eventsSvg.classList.add("clicked-svg");
+    wishlistText.classList.remove("clicked");
+    wishlistSvg.classList.remove("clicked-svg");
+});
+
+wishlistButton.addEventListener("click", function() {
+    console.log("Wishlist werkt");
+    upcomingEvents.classList.add("hidden");
+    savedEvents.classList.remove("hidden");
+    
+    wishlistText.classList.add("clicked");
+    wishlistSvg.classList.add("clicked-svg");
+    eventsText.classList.remove("clicked");
+    eventsSvg.classList.remove("clicked-svg");
 });
