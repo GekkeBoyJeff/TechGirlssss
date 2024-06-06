53932a4c789f733fd01144cfd866fe0d28ccefaa
Abby de Best <121155006+abbydebest@users.noreply.github.com>
2024-06-06 17:25:12 +0200

    More JS with classlist en .hidden

--- static/js/switchButtons.js
+++ static/js/switchButtons.js
@@ -1,8 +1,13 @@
-console.log('test')
+console.log('test');
 
-let wishlist = document.querySelector(".saved-events");
+let wishlistButton = document.querySelector(".saved-events-button");
+let upcomingEventsButton = document.querySelector(".upcoming-events-button");
+
+let savedEvents = document.querySelector(".saved-events");
 let upcomingEvents = document.querySelector(".upcoming-events");
 
-function switchButtons (params) {
-    console.log('switch')
-}
+upcomingEventsButton.addEventListener("click", function() {
+    console.log("Switch werkt");
+    upcomingEvents.classList.remove(".hidden");
+    savedEvents.classList.add(".hidden");
+});
