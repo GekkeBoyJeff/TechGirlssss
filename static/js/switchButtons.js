8f289f627e84e4383d5cade7b11216310f6b638e
Abby de Best <121155006+abbydebest@users.noreply.github.com>
2024-06-06 16:02:56 +0200

    Started on the switch buttons JS

---
+++ static/js/switchButtons.js
@@ -0,0 +1,8 @@
+console.log('test')
+
+let wishlist = document.querySelector(".saved-events");
+let upcomingEvents = document.querySelector(".upcoming-events");
+
+function switchButtons (params) {
+    console.log('switch')
+}
