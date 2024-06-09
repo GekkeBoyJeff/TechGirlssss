c23a325099f4d47f07f2dfa18cc2a5e7d55f2dec
Abby de Best <121155006+abbydebest@users.noreply.github.com>
2024-06-09 19:33:47 +0200

    Set default switch buttons colour to grey and changed colour and stroke width on click with JS

--- static/js/switchButtons.js
+++ static/js/switchButtons.js
@@ -9,18 +9,21 @@ let upcomingEvents = document.querySelector(".upcoming-events");
 let wishlistText = document.querySelector("#wishlistText");
 let eventsText = document.querySelector("#eventsText");
 
-let wishlistSvg = document.querySelector("#wishlistSvg");
-let eventsSvg = document.querySelector("#eventsSvg");
+let wishlistSvg = document.querySelector(".wishlistSvg");
+let svgElements = document.querySelectorAll(".eventsSvg");
 
 upcomingEventsButton.addEventListener("click", function() {
     console.log("Upcoming events werkt");
     upcomingEvents.classList.remove("hidden");
     savedEvents.classList.add("hidden");
     
-    // eventsText.classList.add("clicked");
-    // eventsSvg.classList.add("clicked-svg");
-    // wishlistText.classList.remove("clicked");
-    // wishlistSvg.classList.remove("clicked-svg");
+    eventsText.classList.add("clicked");
+
+    svgElements.forEach(function(elements) {
+        elements.classList.add(".clicked-svg")
+    })
+    wishlistText.classList.remove("clicked");
+    wishlistSvg.classList.remove("clicked-svg");
 });
 
 wishlistButton.addEventListener("click", function() {
@@ -28,8 +31,8 @@ wishlistButton.addEventListener("click", function() {
     upcomingEvents.classList.add("hidden");
     savedEvents.classList.remove("hidden");
     
-    // wishlistText.classList.add("clicked");
-    // wishlistSvg.classList.add("clicked-svg");
-    // eventsText.classList.remove("clicked");
-    // eventsSvg.classList.remove("clicked-svg");
+    wishlistText.classList.add("clicked");
+    wishlistSvg.classList.add("clicked-svg");
+    eventsText.classList.remove("clicked");
+    eventsSvg.classList.remove("clicked-svg");
 });
