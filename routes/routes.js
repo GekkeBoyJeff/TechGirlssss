774eb9d2d9a5772cf9e3da6c6d7acb52ad400378
= <jeffrey.ullers@hva.nl>
2024-06-13 10:59:20 +0200

    added some fetches and added the data to pages

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 import { registerUser, loginUser, logoutUser } from '../controllers/userController.js'
 import { errorReporter } from '../controllers/formErrorHandler.js'
 import { welcomeHandler } from '../controllers/welcomeHandler.js'
-import { fetchPopularEvents, fetchSpecificEvent } from '../api/fetchEventbride.js'
+import { fetchPopularEvents, fetchSpecificEvent, fetchApi } from '../api/fetchEventbride.js'
 
 export const routes = {
     '/': { view: 'pages/index', title: 'Home of techGirlsss', layouts: 'default.ejs', scripts: ['script', 'test'], fetches:[fetchPopularEvents], functions: [], errors: [] },
@@ -12,6 +12,6 @@ export const routes = {
     '/logout': { title: 'Logout', scripts: [], functions: [logoutUser], errors: [], onlyPost: true,fetches:[] },
     '/welcome': { view: 'pages/welcome', title: 'Introduction', scripts: [], functions: [welcomeHandler], errors: [], requiresAuth: true,fetches:[] },
     '/profile': { view: 'pages/profile', title: 'Profile', scripts: [], functions: [], errors: [], requiresAuth: true,fetches:[] },
-    '/events': { view: 'pages/eventsWorkshops', title: 'Events & workshops', scripts: [], functions: [], errors: [], apiData:[],fetches:[] },
-    '/detailevent/:id': { view: 'pages/detailEvent', title: 'Event', scripts: [], functions: [], errors: [], fetches:[fetchSpecificEvent] },
+    '/events': { view: 'pages/eventsWorkshops', title: 'Events & workshops', scripts: [], functions: [], errors: [], apiData:[],fetches:[fetchApi] },
+    '/events/:id': { view: 'pages/detailEvent', title: 'Event', scripts: [], functions: [], errors: [], fetches:[fetchSpecificEvent] },
 }
