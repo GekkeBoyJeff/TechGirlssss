f4d8a79eda1c3f96e532047b013c71407cf76e5b
Abby de Best <121155006+abbydebest@users.noreply.github.com>
2024-06-13 19:10:48 +0200

    JS button

--- static/js/heartButton.js
+++ static/js/heartButton.js
@@ -1,1 +1,13 @@
-let heartButtonClick = document.querySelectorAll("heartButton");
+let heartButtonClick = document.querySelectorAll(".heartButton");
+
+heartButtonClick.forEach(button => {
+    button.addEventListener('click', () => {
+        console.log('heart button werkt');
+
+        let heartIcon = button.querySelector("heartIcon");
+        let heartPath = button.querySelector("heartPath");
+
+        heartButtonIcon.classList.add("heartButtonIsClicked");
+        heartButtonPath.classList.add("heartButtonIsClicked");
+})
+})
